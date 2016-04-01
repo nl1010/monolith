984c2f00d15d54ed44b08e340998a0a0ed56f729
Yan <yan@Yans-MacBook-Pro.local>
2016-04-01 15:50:40 +0800

    modified #2

--- breath_lighting/breath_lighting.ino
+++ breath_lighting/breath_lighting.ino
@@ -1,21 +1,37 @@
+//Libraries 
+#include <Wire.h>
+
+
+//Global Vars 
+int BREATH_LED_PIN = 5;
+int MAX_LED_STRENGTH = 50; //MAX = 255 
+int MIN_LED_STRENGTH = 1;
+int BREATH_TIME = 3000; //in ms, change for a single breath time , including full cycle
+int BREATH_GAP = 2000 ; //in ms, gap between two breathing time
+//Linked Vars 
+int BREATH_STEP = (BREATH_TIME / (MAX_LED_STRENGTH - MIN_LED_STRENGTH))/2;
+
+//Settings
 void setup() {
 
-   pinMode(5,OUTPUT);
+
+   pinMode(BREATH_LED_PIN,OUTPUT);
   
 }
 
+//Main
 void loop() {
-  for (int i=0; i<=255; i++)
+  for (int i= MIN_LED_STRENGTH; i<=MAX_LED_STRENGTH; i++)
   {
-    analogWrite(5,i);
-    delay(14);
+    analogWrite(BREATH_LED_PIN,i);
+    delay(BREATH_STEP);
   }
   
-  for (int i=255; i>=0; i--)
+  for (int i=MAX_LED_STRENGTH; i>=MIN_LED_STRENGTH; i--)
   {
-    analogWrite(5,i);
-    delay(14);
+    analogWrite(BREATH_LED_PIN,i);
+    delay(BREATH_STEP);
   }
-  delay(800);
+  delay(BREATH_GAP);
 
 }     
