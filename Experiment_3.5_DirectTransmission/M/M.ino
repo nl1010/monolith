8528e00d0adefd703a4eb072b76b2b886163af18
Yan <yan@Yans-MacBook-Pro.local>
2016-04-03 23:43:34 +0800

    detect other device successful

---
+++ Experiment_3.5_DirectTransmission/M/M.ino
@@ -0,0 +1,12 @@
+#include <Wire.h>
+
+void setup() {
+   Wire.begin(10);
+     
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- Experiment_4/M10/M10.ino
+++ Experiment_4/M10/M10.ino
@@ -8,6 +8,9 @@ int MIN_LED_STRENGTH = 1;
 int BREATH_TIME = 3000; //in ms, change for a single breath time , including full cycle
 int BREATH_GAP = 2000 ; //in ms, gap between two breathing time
 
+//Tokens
+int arrange_token = 0; //everytime device inits the token is 0 
+
 //vvvvvvv M-S Table -- shared to every module vvvvvvvv//
 int MASTER_ID = 10;
 int SLAVE1_ID = 11;  
@@ -34,26 +37,6 @@ void loop() {
        led_breath(BREATH_LED_PIN); //let's flash
     }
   }
-
-
-  Wire.requestFrom(SLAVE2_ID,1);
-  while(Wire.available()>0) {
-    char sig = Wire.read();
-    Serial.print(sig);
-    if (sig == '2') {
-       led_breath(BREATH_LED_PIN); //let's flash
-    }
-  }  
-
-
-  Wire.requestFrom(SLAVE3_ID,1);
-  while(Wire.available()>0) {
-    char sig = Wire.read();
-    Serial.print(sig);
-    if (sig == '3') {
-       led_breath(BREATH_LED_PIN); //let's flash
-    }
-  } 
 }
 
 
